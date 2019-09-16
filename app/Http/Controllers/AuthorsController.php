ae884fb55b6eaed890407e38141e691fd560856f
Jan Polzer <janman74@gmail.com>
2019-09-16 16:42:58 -0500

    Working progress for Authors and change route to authors (from author)

--- app/Http/Controllers/AuthorsController.php
+++ app/Http/Controllers/AuthorsController.php
@@ -9,8 +9,15 @@ class AuthorsController extends Controller
 {
     public function store()
     {
-        Author::create(request()->only([
-            'name', 'dob',
-        ]));
+        Author::create($this->validateRequest());
+    }
+
+    // helper methods
+    protected function validateRequest()
+    {
+        return request()->validate([
+            'name' => 'required',
+            'dob' => 'required',
+        ]);
     }
 }
--- tests/Feature/AuthorManagementTest.php
+++ tests/Feature/AuthorManagementTest.php
@@ -18,15 +18,39 @@ class AuthorManagementTest extends TestCase
     {
         $this->withoutExceptionHandling();
 
-        $this->post('/author',[
-            'name' => 'John Doe',
-            'dob' => '01/01/1970',
-        ]);
+        $this->post('/authors', $this->data());
 
         $author = Author::all();
 
         $this->assertCount(1, $author);
         $this->assertInstanceOf(Carbon::class, $author->first()->dob);
         $this->assertEquals('1970/01/01', $author->first()->dob->format('Y/d/m'));
+    }
+
+    /** @test */
+    public function a_name_is_required()
+    {
+        $response = $this->post('/authors', array_merge($this->data(), ['name' => '']));
+
+        $response->assertSessionHasErrors('name');
+    }
+
+    /** @test */
+    public function a_dob_is_required()
+    {
+        $response = $this->post('/authors', array_merge($this->data(), ['dob' => '']));
+
+        $response->assertSessionHasErrors('dob');
+    }
+
+    /**
+     * @return array
+     */
+    public function data(): array
+    {
+        return [
+            'name' => 'John Doe',
+            'dob' => '01/01/1970',
+        ];
     }
 }
