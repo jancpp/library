a320536279ea312926570a77e7eaa263b41cfa01
Jan Polzer <janman74@gmail.com>
2019-09-17 10:35:47 -0500

    Add create to Authors

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,11 @@ Route::post('/books', 'BooksController@store');
 Route::patch('/books/{book}', 'BooksController@update');
 Route::delete('/books/{book}', 'BooksController@destroy');
 
+Route::get('/authors/create', 'AuthorsController@create');
 Route::post('/authors', 'AuthorsController@store');
 
 Route::post('/checkout/{book}', 'CheckoutBookController@store');
 Route::post('/checkin/{book}', 'CheckinBookController@store');
-
-
 
 Auth::routes();
 
